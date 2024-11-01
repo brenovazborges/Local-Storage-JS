64a850b952be976a3934a3e754ae4f3fcc69c270
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-10-31 22:04:29 -0300

    finalizado

---
+++ JS/script.js
@@ -0,0 +1,56 @@
+let chave = "Nome 1";
+let chave2 = "Email 1";
+let i = 0;
+
+
+function ArmazenaDados()
+{
+    localStorage.setItem(chave, document.getElementById("nome").value);
+    localStorage.setItem(chave2, document.getElementById("email").value);
+    LimparDados();
+}
+
+function ConsultaDados()
+{
+    document.getElementById("resultado").innerHTML = localStorage.getItem(chave) + " - " + localStorage.getItem(chave2);
+}
+
+function LimparDados()
+{
+    document.getElementById("nome").value = "";
+    document.getElementById("email").value = "";
+}
+
+function AtualizaDados ()
+{
+    localStorage.setItem(chave, document.getElementById("nome").value);
+    localStorage.setItem(chave2, document.getElementById("email").value);
+    LimparDados();
+}
+
+function InsertData()
+{
+    i = i + 1;
+    localStorage.setItem("Nome " + i, document.getElementById("nome").value);
+    localStorage.setItem("Email " + i, document.getElementById("email").value);
+    LimparDados();
+}
+
+function ViewAllData()
+{
+    let resultado = "";
+
+    for (let indice = 1; indice <= i; indice++)
+    {
+        resultado += localStorage.getItem("Nome " + indice) + " - " + localStorage.getItem("Email " + indice) + "<br>"; 
+    }
+
+    document.getElementById("resultado").innerHTML = resultado;
+}
+
+function DeleteData()
+{
+    localStorage.removeItem("Nome " + document.getElementById("indice").value);
+    localStorage.removeItem("Email " + document.getElementById("indice").value);
+    document.getElementById("indice").value = "";
+}
